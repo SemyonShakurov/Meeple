b448e52f828e1369ef68d9781b6e3663665ad15f
Semyon <ssshakurov@edu.hse.ru>
2021-02-25 14:41:38 +0700

    Firebase delete

--- app/src/main/java/com/mscorp/meeple/AuthenticationActivity.kt
+++ app/src/main/java/com/mscorp/meeple/AuthenticationActivity.kt
@@ -2,18 +2,11 @@ package com.mscorp.meeple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class AuthenticationActivity : AppCompatActivity() {
-
-    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
-
-        auth = Firebase.auth
     }
 }
